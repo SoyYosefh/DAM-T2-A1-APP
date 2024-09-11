42a823130471f594237817cf94bccee955c1d297
yosefhdev <yosefhknt@gmail.com>
2024-09-11 16:24:38 -0700

    Fix de blob para que puncione con useEffect

--- src/functions/MyFetchBlob.jsx
+++ src/functions/MyFetchBlob.jsx
@@ -1,35 +1,39 @@
+import { useEffect } from "react";
+
 function MyFetchBlob() {
 
-    // Obtiene referencias a los elementos de imagen por sus ID.
-    let img1 = document.getElementById('IdImg1');
-    let img2 = document.getElementById('IdImg2');
+    useEffect(() => {
+        // Obtiene referencias a los elementos de imagen por sus ID después de que el componente se ha montado.
+        let img1 = document.getElementById('IdImg1');
+        let img2 = document.getElementById('IdImg2');
 
-    // Realiza una solicitud GET para obtener el primer archivo de imagen como un blob.
-    fetch('images/MazingerZ-1.png')
-        .then(resp => resp.blob()) // Convierte la respuesta de la solicitud en un objeto Blob.
-        .then(image => {
-            console.log(image); // Imprime el Blob en la consola para inspección.
-            // Crea una URL de objeto a partir del Blob y asigna esta URL a la fuente de la primera imagen.
-            var imgPath1 = URL.createObjectURL(image);
-            img1.src = imgPath1;
-        });
+        // Realiza una solicitud GET para obtener el primer archivo de imagen como un blob.
+        fetch('images/MazingerZ-1.png')
+            .then(resp => resp.blob()) // Convierte la respuesta de la solicitud en un objeto Blob.
+            .then(image => {
+                console.log(image); // Imprime el Blob en la consola para inspección.
+                // Crea una URL de objeto a partir del Blob y asigna esta URL a la fuente de la primera imagen.
+                var imgPath1 = URL.createObjectURL(image);
+                img1.src = imgPath1;
+            });
 
-    // Realiza una solicitud GET para obtener el segundo archivo de imagen como un blob.
-    fetch('images/MazingerZ-2.png')
-        .then(resp => {
-            return resp.blob(); // Convierte la respuesta de la solicitud en un objeto Blob.
-        })
-        .then(image => {
-            console.log(image); // Imprime el Blob en la consola para inspección.
-            // Crea una URL de objeto a partir del Blob y asigna esta URL a la fuente de la segunda imagen.
-            var imgPath2 = URL.createObjectURL(image);
-            img2.src = imgPath2;
-        });
+        // Realiza una solicitud GET para obtener el segundo archivo de imagen como un blob.
+        fetch('images/MazingerZ-2.png')
+            .then(resp => resp.blob()) // Convierte la respuesta de la solicitud en un objeto Blob.
+            .then(image => {
+                console.log(image); // Imprime el Blob en la consola para inspección.
+                // Crea una URL de objeto a partir del Blob y asigna esta URL a la fuente de la segunda imagen.
+                var imgPath2 = URL.createObjectURL(image);
+                img2.src = imgPath2;
+            });
+    }, []); // El segundo argumento vacío asegura que el efecto se ejecute solo una vez después del montaje.
+    // Llama a la función para cargar las imágenes.
+
 
     return (
         <div className="flex flex-col items-center h-screen w-full p-10">
             <h1 className="text-5xl mb-10">
-                Programa para FETCH API PUT.
+                Programa para <br /> FETCH API PUT.
             </h1>
             <div className="flex gap-2">
                 {/* Elementos de imagen que mostrarán las imágenes cargadas. */}
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 // import App from './App.jsx'
-// import MyFunctions from "./functions/MyFunctions.jsx";
-// import MyPromises from './functions/MyPromises.jsx';
+
+// import { MyPromises } from './functions/MyPromises'
 // import MyPromisesAll from './functions/MyPromisesAll.jsx';
 // import MyPrimesesRace from './functions/MyPrimesesRace.jsx'
 // import MyFetchAPIs from './functions/MyFetchAPIs.jsx'
 // import MyFetchPost from './functions/MyFetchPost'
+// import MyFetchPut from './functions/MyFetchPut'
 import MyFetchBlob from './functions/MyFetchBlob'
 
 createRoot(document.getElementById('root')).render(
@@ -20,6 +21,7 @@ createRoot(document.getElementById('root')).render(
     {/* <MyPrimesesRace /> */}
     {/* <MyFetchAPIs /> */}
     {/* <MyFetchPost /> */}
+    {/* <MyFetchPut /> */}
     <MyFetchBlob />
   </StrictMode>
 )
